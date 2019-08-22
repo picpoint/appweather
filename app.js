faf64308d7f8764e4797f86736223520f4dc8a9f
picpoint <picpoint@yandex.ru>
2019-08-22 16:57:33 +0300

    work with service

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.port || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Home Page !');
+  res.render('index.html');
 });
 
 
--- src/js/reqCity.js
+++ src/js/reqCity.js
@@ -1,13 +1,19 @@
 const input = document.querySelector('.general__findblock').firstChild;
 const btn = document.querySelector('.general__findblock').lastChild;
+const span = document.querySelector('.general__datablock').firstChild;
+const nameCity = document.querySelector('.general__nameCity');
+const visibility = document.querySelector('.general__visibility');
 const key = 'b13322108a6798e54b547c8d1708f1c2';
 const uri = 'http://api.openweathermap.org/data/2.5/weather?q=';
-let result = '';
+
 
 /*
 simple work template
 https://api.openweathermap.org/data/2.5/weather?q=Stavropol&appid=b13322108a6798e54b547c8d1708f1c2
 */
+
+console.log(nameCity);
+console.log(visibility);
 
 function responseStr () {
   const cityName = input.value;
@@ -17,13 +23,29 @@ function responseStr () {
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.addEventListener('readystatechange', () => {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      result = xhr.responseText;
+      let result = xhr.responseText;
       //console.log(result);
+      let objJson = JSON.parse(result);
+      //console.log(objJson);
+
+      for (let key in objJson) {
+        //console.log(key);
+        if (key == 'name') {
+          console.log(objJson[key]);
+          nameCity.innerText = objJson[key];
+        }
+
+        if (key == 'visibility') {
+          visibility.innerText = objJson[key];
+        }
+
+
+      }
+
     }
   });
   xhr.send();
 
-  console.log(result);
 
 }
 

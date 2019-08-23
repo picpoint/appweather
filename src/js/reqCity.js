17b75a983aa4bcb67196e5d3b56236d1b4751b9e
picpoint <picpoint@yandex.ru>
2019-08-23 16:54:09 +0300

    work to functionals

--- src/js/reqCity.js
+++ src/js/reqCity.js
@@ -6,6 +6,9 @@ const visibility = document.querySelector('.general__visibility');
 const tempCity = document.querySelector('.general__temp');
 const pressCity = document.querySelector('.general__press');
 const humidCity = document.querySelector('.general__humid');
+const windCity = document.querySelector('.general__wnd');
+const cloudsCity = document.querySelector('.general__clds');
+const visibilityCity = document.querySelector('.general__vsblt');
 
 
 const key = 'b13322108a6798e54b547c8d1708f1c2';
@@ -23,17 +26,28 @@ function responseStr () {
       let result = xhr.responseText;
       let objJson = JSON.parse(result);
       console.log(objJson);
-
       for (let key in objJson) {
+        //console.log(objJson[key]);
         if (key == 'name') {
           nameCity.innerText = objJson[key];
         }
 
         if (key == 'main') {
-          console.log(objJson[key].temp);
           tempCity.innerHTML = Math.floor(objJson[key].temp - 273.15) + ' C';
-          pressCity.innerHTML = objJson[key].pressure + 'мм. рт. ст.';
+          pressCity.innerHTML = objJson[key].pressure - 300 + ' мм. рт. ст.';
           humidCity.innerHTML = objJson[key].humidity + ' %';
+        }
+
+        if (key == 'wind') {
+          windCity.innerHTML = objJson[key].speed + ' м/с';
+        }
+
+        if (key == 'clouds') {
+          cloudsCity.innerHTML = objJson[key].all + ' %';
+        }
+
+        if (key == 'visibility') {
+          visibilityCity.innerHTML = objJson[key] + ' м.';
         }
 
 

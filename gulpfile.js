7903f3298b99b91cabaf71b1946c9337100401dd
rmtar <picpoint@yandex.ru>
2019-08-29 22:08:49 +0300

    make adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,48 @@ const smartgrid = require('smart-grid');
 const browserSync = require('browser-sync').create();
 
 
+const smartGrigConf = {
+  outputStyle: 'less',
+  colums: 12,
+  offset: '30px',   // межклоночник
+  mobileFirst: false,
+  container: {
+    maxWidth: '1280px',
+    fields: '30px'   // отступ от края экрана
+  },
+  breakPoints: {
+    mgxl: {
+      width: '1800px',
+      fields: '30px'
+    },
+    xxl: {
+      width: '1400px',
+      fields: '30px'
+    },
+    xl: {
+      width: '1140px',
+      fields: '30px'
+    },
+    lg: {
+      width: '960px',
+      fields: '30px'
+    },
+    md: {
+      width: '720px',
+      fields: '20px'
+    },
+    sm: {
+      width: '540px',
+      fields: '10px'
+    },
+    xs: {
+      width: '480px',
+      fields: '5px'
+    }
+  }
+};
+
+
 
 gulp.task('less', () => {
   return gulp.src('./src/less/style.less')
@@ -34,7 +76,9 @@ gulp.task('js', () => {
     }));
 });
 
-
+gulp.task('grid', () => {
+  smartgrid('./src/less', smartGrigConf);
+});
 
 gulp.task('watch', ['less', 'htmlmin', 'js', 'browserSync'], () => {
   gulp.watch('./src/less/style.less', ['less']);
